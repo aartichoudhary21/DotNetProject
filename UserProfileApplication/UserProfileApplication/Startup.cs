577c37c3776df96d64180a2c209a5a640aaa5a5c
SK <titanic.love.sk@gmail.com>
2016-11-25 21:28:11 -0800

    Default form commit

---
+++ UserProfileApplication/UserProfileApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UserProfileApplication.Startup))]
+namespace UserProfileApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
